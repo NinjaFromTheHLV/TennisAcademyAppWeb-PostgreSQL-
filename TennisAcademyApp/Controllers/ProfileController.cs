1a323ea93e0549b2b0cd957fda45ba96c70d1852
Yordan Pavlov <iordan_pavlov1@abv.bg>
2026-05-23 22:08:46 +0300

    Profile pic custom view

--- TennisAcademyApp/Controllers/ProfileController.cs
+++ TennisAcademyApp/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -7,40 +8,53 @@ using System.IO;
 using System.Threading.Tasks;
 using TennisAcademyApp.Data.Models;
 
-public class ProfileController : Controller
+namespace TennisAcademyApp.Controllers
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly IWebHostEnvironment _webHostEnvironment;
-
-    public ProfileController(UserManager<ApplicationUser> userManager, IWebHostEnvironment webHostEnvironment)
+    [Authorize]
+    public class ProfileController : Controller
     {
-        _userManager = userManager;
-        _webHostEnvironment = webHostEnvironment;
-    }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-    [HttpPost]
-    public async Task<IActionResult> UploadPicture(IFormFile profilePicture)
-    {
-        if (profilePicture != null && profilePicture.Length > 0)
+        public ProfileController(UserManager<ApplicationUser> userManager, IWebHostEnvironment webHostEnvironment)
+        {
+            _userManager = userManager;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ChangePicture()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null) return NotFound();
 
-            string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images", "users");
-            Directory.CreateDirectory(uploadsFolder);
-
-            string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(profilePicture.FileName);
-            string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-            using (var fileStream = new FileStream(filePath, FileMode.Create))
-            {
-                await profilePicture.CopyToAsync(fileStream);
-            }
-
-            user.ProfilePictureUrl = $"/images/users/{uniqueFileName}";
-            await _userManager.UpdateAsync(user);
+            return View(user);
         }
 
-        return RedirectToAction("Index");
+        [HttpPost]
+        public async Task<IActionResult> UploadPicture(IFormFile profilePicture)
+        {
+            if (profilePicture != null && profilePicture.Length > 0)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null) return NotFound();
+
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images", "users");
+                Directory.CreateDirectory(uploadsFolder);
+
+                string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(profilePicture.FileName);
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await profilePicture.CopyToAsync(fileStream);
+                }
+
+                user.ProfilePictureUrl = $"/images/users/{uniqueFileName}";
+                await _userManager.UpdateAsync(user);
+            }
+
+            return RedirectToAction("ChangePicture");
+        }
     }
 }
